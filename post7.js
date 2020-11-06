dd4328badc586f853deca59d2d04ca30c7c6c5af
handcg1 <handcg@dukes.jmu.edu>
2020-11-06 09:15:16 -0500

    post 7

---
+++ post7.js
@@ -0,0 +1,14 @@
+  // Function to roll the dice 
+  function rollTheDice() { 
+    setTimeout(function () { 
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+
+        document.querySelector(".img1").setAttribute("src", 
+            "dice" + randomNumber1 + ".png"); 
+
+        document.querySelector(".img2").setAttribute("src", 
+            "dice" + randomNumber2 + ".png"); 
+
+    }, 1000); 
+} 
